2592ac2e69cfc726d3243755a33ef8da3bd64eb1
bwardak <baheer.wardak06@gmail.com>
2024-04-03 14:29:25 +0100

    Added extra to description section. Making description length shorter.

--- src/components/BeerContainer/BeerContainer.tsx
+++ src/components/BeerContainer/BeerContainer.tsx
@@ -8,19 +8,17 @@ type BeerContainerProps = {
 
 
 const BeerContainer = ({searchBeers}: BeerContainerProps) => {
+  
 
   return (
     <div className="beer-tiles-container">
       {searchBeers.map((beer) => (
         <BeerTiles
           key={beer.id}
-          image={
-            beer.image_url ??
-            "https://media.istockphoto.com/id/520229130/photo/bottle-of-beer-on-white-background.jpg?s=612x612&w=0&k=20&c=pjleW3VBHs2dSVh95YtSkjIBrdLGkhkZ0fW4LWPznbM="
-          }
+          image={beer.image_url ??
+            "https://media.istockphoto.com/id/520229130/photo/bottle-of-beer-on-white-background.jpg?s=612x612&w=0&k=20&c=pjleW3VBHs2dSVh95YtSkjIBrdLGkhkZ0fW4LWPznbM="}
           name={beer.name}
-          description={beer.description}
-        />
+          description={beer.description} tagline={beer.tagline} brewDate={beer.first_brewed} abv={beer.abv} ph={beer.ph}        />
       ))}
     </div>
   );
--- src/components/BeerTiles/BeerTiles.tsx
+++ src/components/BeerTiles/BeerTiles.tsx
@@ -4,17 +4,55 @@ type Beers = {
   image: string;
   name: string;
   description: string;
+  tagline: string;
+  brewDate: string;
+  abv: number;
+  ph: number | null;
 }
 
-const BeerTiles = ({image, name, description}: Beers) => {
+const BeerTiles = ({image, name, description, tagline, brewDate, abv, ph}: Beers) => {
+
+  let beerDescription: string[] = description.split(".");
+let truncatedBeerDescription: string = "";
+let totalLength: number = 0;
+
+for (let i: number = 0; i < beerDescription.length; i++) {
+    const sentence = beerDescription[i].trim() + ".";
+    if (totalLength + sentence.length <= 300) {
+        truncatedBeerDescription += sentence;
+        totalLength += sentence.length;
+    } else {
+        break;
+    }
+}
+
+  console.log(beerDescription[0].length);
   
+
+  // if (truncatedBeerDescription.length >= 260) {
+  //   truncatedBeerDescription = `${truncatedBeerDescription.slice(0, 250)}... `;
+  // }
+
+  // if (truncatedBeerDescription === "") {
+  //   truncatedBeerDescription = `${description.slice(0, 250)}... `
+  // }
+  
+
   return (
     <div className="beers">
-      <img className='beers__image' src={image} alt="Beer" />
-      <h1 className='beers__name'>{name}</h1>
-      <p className='beers__description'>{description}</p>
+      <div className="beers__image-background">
+        <img className="beers__image" src={image} alt="Beer" />
+      </div>
+      <div className="beers__info">
+        <h1 className="beers__name">{name}</h1>
+        <h3 className="beers__tagline">{tagline}</h3>
+        <p className="beers__brew-date">Brew Date: {brewDate}</p>
+        <p className="beers__abv">ABV: {abv}</p>
+        <p className="beers__ph">pH: {ph}</p>
+        <p className="beers__description">{truncatedBeerDescription}</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default BeerTiles
